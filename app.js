36f75846c86d650202954d7a81ab7c97e17ba3f4
Samuel Pedro <samuelcpedro@gmail.com>
2019-09-19 10:25:50 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize('seq_db', 'root', 'password', {
+  host: 'localhost',
+  dialect: 'mysql'
+})
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.')
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err)
+  })
+
+const User = sequelize.define('user', {
+  // attributes
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING
+    // allowNull defaults to true
+  }
+}, {
+  // options
+})
+
+// Note: using `force: true` will drop the table if it already exists
+User.sync({ force: true }).then(() => {
+  // Now the `users` table in the database corresponds to the model definition
+  return User.create({
+    firstName: 'John',
+    lastName: 'Hancock'
+  })
+})
